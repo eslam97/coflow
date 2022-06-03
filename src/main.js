8a11b453ce80dff8205d5cc544436c0f42cee708
eslam <e01095097908@gmail.com>
2022-06-04 00:15:28 +0200

    view_modal

--- src/main.js
+++ src/main.js
@@ -5,10 +5,7 @@ import store from './store'
 import i18n from './i18n'
 import './helper'
 import './components'
-import 'vue-slick-carousel/dist/vue-slick-carousel.css'
-import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
-import VueSlickCarousel from 'vue-slick-carousel'
-Vue.component('VueSlickCarousel', VueSlickCarousel)
+
 Vue.config.productionTip = false
 
 const vm = new Vue({

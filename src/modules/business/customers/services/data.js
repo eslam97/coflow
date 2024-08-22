60d30d00013214dea82049193e70aed7706f957f
Mohamed Elsohagy <m.elsohagy@dashcode.co>
2024-08-22 11:43:00 +0300

    Finish Customer Activity module

--- src/modules/business/customers/services/data.js
+++ src/modules/business/customers/services/data.js
@@ -17,3 +17,21 @@ export const purchasessItems = [
   { id: 6, admin: 'Amgd Ali', date: '2023-4-21', customer: 'Omar Mohamed', action: 'Refund', purchase: 'Ticket', name: 'Ticket Name', amount: '1000 (EGP)', status: 'FOP' },
   { id: 7, admin: 'Omar Ahmed', date: '2023-1-21', customer: 'Sara Ahmed', action: 'Expiration', purchase: 'Package', name: 'Offer Name', amount: '1000 (EGP)', status: 'FOP' }
 ]
+export const giftsItems = [
+  { id: 1, admin: 'Ali Ahmed', date: '2023-10-21', customer: 'Mohamed Ahmed', gift: 'Ticket', name: 'Offer Name', quantity: '2' },
+  { id: 2, admin: 'Ahmed Amr', date: '2023-9-21', customer: 'Ahmed Mohamed', gift: 'Ticket', name: 'Offer Name', quantity: '3' },
+  { id: 3, admin: 'Amr Ali', date: '2023-8-11', customer: 'Amir Mohamed', gift: 'Ticket', name: 'Offer Name', quantity: '4' },
+  { id: 4, admin: 'Customer', date: '2023-7-10', customer: 'Sara Ahmed', gift: 'Coupon', name: 'Ticket Name', quantity: '1' },
+  { id: 5, admin: 'Customer', date: '2023-5-10', customer: 'Alaa Mohamed', gift: 'Coupon', name: 'Ticket Name', quantity: '1' },
+  { id: 6, admin: 'Amgd Ali', date: '2023-4-21', customer: 'Omar Mohamed', gift: 'Ticket', name: 'Ticket Name', quantity: '2' },
+  { id: 7, admin: 'Omar Ahmed', date: '2023-1-21', customer: 'Sara Ahmed', gift: 'Coupon', name: 'Offer Name', quantity: '1' }
+]
+export const collectionsItems = [
+  { id: 1, admin: 'Ali Ahmed', date: '2023-10-21', customer: 'Mohamed Ahmed', action: 'Reservation', service: 'Flow', name: 'Offer Name' },
+  { id: 2, admin: 'Ahmed Amr', date: '2023-9-21', customer: 'Ahmed Mohamed', action: 'Reservation', service: 'Activity', name: 'Offer Name' },
+  { id: 3, admin: 'Amr Ali', date: '2023-8-11', customer: 'Amir Mohamed', action: 'Reservation', service: 'Course', name: 'Offer Name' },
+  { id: 4, admin: 'Customer', date: '2023-7-10', customer: 'Sara Ahmed', action: 'Collection', service: 'Flow', name: 'Ticket Name' },
+  { id: 5, admin: 'Customer', date: '2023-5-10', customer: 'Alaa Mohamed', action: 'Collection', service: 'Activity', name: 'Ticket Name' },
+  { id: 6, admin: 'Amgd Ali', date: '2023-4-21', customer: 'Omar Mohamed', action: 'Collection', service: 'Course', name: 'Ticket Name' },
+  { id: 7, admin: 'Omar Ahmed', date: '2023-1-21', customer: 'Sara Ahmed', action: 'Collection', service: 'Flow', name: 'Offer Name' }
+]

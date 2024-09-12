b8c7ee8551c897d5e6ed3b48942a8b2ee224ea1c
Mohamed Elsohagy <m.elsohagy@dashcode.co>
2024-09-12 14:55:04 +0300

    Create layout of Facility

---
+++ src/modules/business/facility/views/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <b-container fluid>
+    <h3>Facility</h3>
+
+    <div class="my-2 d-flex justify-content-between align-items-center gap-2">
+      <router-link :to="{ name: 'facility.profile' }" :class="['w-100 text-center promotion_button py-2', { 'active-tab' : $route.name =='facility.profile'}]">
+        Profile
+      </router-link>
+      <router-link :to="{ name: 'facility.faqs' }" :class="['w-100 text-center promotion_button py-2', { 'active-tab' : $route.name =='facility.faqs'}]">
+        FAQs
+      </router-link>
+      <router-link :to="{ name: 'facility.payments' }" :class="['w-100 text-center promotion_button py-2', { 'active-tab' : $route.name =='facility.payments'}]">
+        Payments
+      </router-link>
+    </div>
+
+    <router-view/>
+  </b-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  components: {
+  },
+  methods: {
+  },
+  mounted () {
+  },
+  created () {
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/modules/business/facility/views/payments.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Payments
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {}
+  },
+  components: {},
+  methods: {},
+  mounted () {},
+  created () {}
+}
+</script>

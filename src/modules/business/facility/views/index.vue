3857c458f60e3f624b5910e2990d4d7086f54456
Mohamed Elsohagy <m.elsohagy@dashcode.co>
2024-09-12 15:25:43 +0300

    Facility

--- src/modules/business/facility/views/index.vue
+++ src/modules/business/facility/views/index.vue
@@ -1,8 +1,20 @@
 <template>
   <b-container fluid>
-    <h3>Facility</h3>
+    <div class="d-flex justify-content-between align-items-center gap-2">
+      <h3>Facility</h3>
 
-    <div class="my-2 d-flex justify-content-between align-items-center gap-2">
+      <div class="temporarily-close">
+        Temporarily Close
+        <div class="custom-control custom-switch custom-switch-text custom-control-inline custom-switch-color mr-0" >
+          <div class="custom-switch-inner">
+            <input type="checkbox" class="custom-control-input bg-info" id="temporarilyClose" v-model="temporarilyClose">
+            <label class="custom-control-label" for="temporarilyClose"></label>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="my-3 d-flex justify-content-between align-items-center gap-2">
       <router-link :to="{ name: 'facility.profile' }" :class="['w-100 text-center promotion_button py-2', { 'active-tab' : $route.name =='facility.profile'}]">
         Profile
       </router-link>
@@ -19,21 +31,32 @@
 </template>
 
 <script>
+import { core } from '@/config/pluginInit'
+
 export default {
   data () {
     return {
+      temporarilyClose: false
     }
   },
   components: {
   },
   methods: {
   },
-  mounted () {
-  },
   created () {
+  },
+  mounted () {
+    core.index()
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.temporarily-close {
+  display: flex;
+  align-items: center;
+  gap: 6px;
+  background-color: #fff;
+  padding: 0.5rem 1rem;
+}
 </style>

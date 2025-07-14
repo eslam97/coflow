61b524ba952a63637f9708a8737650c59999d0a9
eslam-ashraf97 <37964189+eslam97@users.noreply.github.com>
2025-07-14 22:14:42 +0300

    test

--- vue.config.js
+++ vue.config.js
@@ -1,44 +1,25 @@
 const webpack = require('webpack')
-const PrerenderSPAPlugin = require('prerender-spa-plugin')
-const path = require('path')
-
 module.exports = {
   publicPath: '/',
-
   devServer: {
     proxy: 'http://localhost:8000/',
     historyApiFallback: {
       disableDotRule: true
     }
   },
-
-  configureWebpack: config => {
-    // Add plugins
-    config.plugins = [
-      ...(config.plugins || []),
-      new webpack.IgnorePlugin({
-        resourceRegExp: /^\.\/locale$/,
-        contextRegExp: /moment$/
-      })
-    ]
-
-    // Add prerender plugin only in production
-    if (process.env.NODE_ENV === 'production') {
-      config.plugins.push(
-        new PrerenderSPAPlugin({
-          staticDir: path.join(__dirname, 'dist'),
-          routes: ['/', '/business']
-        })
-      )
-    }
-
-    // Merge resolve config safely
-    config.resolve = {
-      ...(config.resolve || {}),
+  configureWebpack: {
+    devServer: {
+      proxy: 'http://localhost:8000/',
+      historyApiFallback: {
+        disableDotRule: true
+      }
+    },
+    plugins: [
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+    ],
+    resolve: {
       alias: {
-        ...(config.resolve?.alias || {}),
-        vue$: 'vue/dist/vue.common.js',
-        '@': path.resolve(__dirname, 'src')
+        vue$: 'vue/dist/vue.common.js'
       }
     }
   },
